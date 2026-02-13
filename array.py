56538b19b71f8c4d548d835a4c1f4041799a9430
vinay <uchihaitachi11329@gmail.com>
2026-02-13 23:43:35 +0530

    arrays basics

---
+++ array.py
@@ -0,0 +1,24 @@
+# in python we can use array in three main ways  list , array  , numpy
+#going with the list as array
+
+#basic array implementation
+arr=[1,2,5,3,6,8]  
+print(arr)
+
+#searching the element based on index position
+arr=[1,2,5,3,6,8]
+print(arr[3])   # --> O(1) time complexity
+
+#traversing through array
+arr=[1,2,5,3,6,8]
+for ele in range(len(arr)):
+    print(arr[ele])  #--> log(n)
+
+#search the specific elements
+arr=[1,2,5,3,6,8,3,7]
+target=6
+def search(arr,target):
+    for i in arr:    #-->log(n)
+        if i==arr:   #-->O(1)
+            return i
+search(arr,target)
