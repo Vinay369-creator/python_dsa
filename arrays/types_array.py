261d663d5aa3d1fa01cc7c4cce85ca75998b82c7
vinay <uchihaitachi11329@gmail.com>
2026-02-19 09:59:12 +0530

    2D arrays examples

--- arrays/types_array.py
+++ arrays/types_array.py
@@ -1,8 +1,8 @@
 #two dimensional array.
 # find the target element in the given 2d array and return the  index position of the element 
-arr=[ [[1,3,5,7],
-       [10,11,16,20],
-       [23,30,34,60]]]
+arr=[[1,3,5,7],
+    [10,11,16,20],
+    [23,30,34,60]]
 target = 3
 
 def targetElement(arr,target):
@@ -17,6 +17,10 @@ print(targetElement(arr,target))
 #ex arr[[1,2,3],             arr =[[7,4,1],
      #  [4,5,6],                  [8,5,2],
      #  [7,8,9]]                  [9,6,3]]
+
+arr=[[1,2,3],
+     [4,5,6],
+     [7,8,9]]
 
 def rotatearr(arr):
     for row in range(len(arr)):
